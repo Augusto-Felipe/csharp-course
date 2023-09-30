63900f9c958bf5a9b6d5b2d43a610d59588ef165
Felipe Augusto <augusto_felipe@icloud.com>
2023-09-30 18:27:24 -0300

    aula sobre operadores de atribuicao

---
+++ aulas/OperadoresDeAtribuicao/OperadoresDeAtribuicao/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace OperadoresDeAtribuicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Operadores de Atribuição
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            string nome = "Felipe";
+            nome += "Augusto";
+
+            Console.WriteLine(nome);
+            #endregion
+
+            #region Operador Aritméticos de Atribuição
+            int b = 10;
+            b++;
+            Console.WriteLine(b);
+
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine();
+
+            int c = 10;
+            int d = c++;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+            int e = 15;
+            Console.WriteLine(++e);
+            #endregion
+
+        }
+    }
+}
