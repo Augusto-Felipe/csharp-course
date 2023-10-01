fd6b35d74dd54c4d4e7a658589e25f240570728d
Felipe Augusto <augusto_felipe@icloud.com>
2023-10-01 18:11:13 -0300

    aula sobre escopo e inicialização de variáveis

---
+++ aulas/EscopoInicializacao/EscopoInicializacao/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EscopoInicializacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Podemos declarar uma variável e depois inicializa-la com um valor
+            int x;
+            x = 10;
+
+            // Podemos declarar uma variável e inicializa-la na mesma linha
+            int y = 20;
+
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = 0.0;
+
+            if (preco > 100.0)
+            {
+                desconto = preco * 0.1;
+            }
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
