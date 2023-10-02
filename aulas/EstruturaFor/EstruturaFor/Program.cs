91fcc3a568f67c8dc731329602ad53d3bd670f3d
Felipe Augusto <augusto_felipe@icloud.com>
2023-10-02 16:36:14 -0300

    estrutura for

---
+++ aulas/EstruturaFor/EstruturaFor/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EstruturaFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos numeros inteiros você vai digitar? ");
+            int qnt = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+
+            for (int i = 0; i < qnt; i++)
+            {
+                Console.Write($"Valor #{i+1}: ");
+                int n = int.Parse(Console.ReadLine());
+                soma += n;
+            }
+
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}
