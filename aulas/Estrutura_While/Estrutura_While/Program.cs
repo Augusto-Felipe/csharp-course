89e0d5b10ccb91385f97b1eee7bf596677b643f6
Felipe Augusto <augusto_felipe@icloud.com>
2023-10-02 14:34:23 -0300

    aula sobre estrutura while

---
+++ aulas/Estrutura_While/Estrutura_While/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Estrutura_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            double n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (n >= 0.0)
+            {
+                double raiz = Math.Sqrt(n);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+
+                Console.Write("Digite outro numero: ");
+                n = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Número negativo!");
+        }
+    }
+}
