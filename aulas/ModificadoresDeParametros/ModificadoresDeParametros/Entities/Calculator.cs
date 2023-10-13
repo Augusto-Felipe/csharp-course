c88f56cac1dfea1546ba673cdbbed1eab83bf95b
Felipe Augusto <augusto_felipe@icloud.com>
2023-10-13 15:59:07 -0300

    modificadores de parametros

---
+++ aulas/ModificadoresDeParametros/ModificadoresDeParametros/Entities/Calculator.cs
@@ -0,0 +1,37 @@
+﻿namespace ModificadoresDeParametros.Entities
+{
+    internal class Calculator
+    {
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+
+        public static void Triplicar(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+
+        public static int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public static int Sum(int n1, int n2, int n3)
+        {
+            return n1 + n2 + n3;
+        }
+
+        public static int Sum(int n1, int n2, int n3, int n4)
+        {
+            return n1 + n2 + n3 + n4;
+        }
+
+    }
+}
---
+++ aulas/ModificadoresDeParametros/ModificadoresDeParametros/Program.cs
@@ -0,0 +1,23 @@
+﻿using ModificadoresDeParametros.Entities;
+
+namespace ModificadoresDeParametros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // PARAMNS
+            int resultado = Calculator.Sum(1, 2, 3, 4, 5, 6);
+            Console.WriteLine("SOMA = " + resultado);
+
+            // REF E OUT
+            int a = 10;
+            int resposta;
+            //Calculator.Triplicar(ref a);
+            //Console.WriteLine(a);
+
+            Calculator.Triplicar(a, out resposta);
+            Console.WriteLine("RESPOSTA = " + resposta);
+        }
+    }
+}
